ccc6711cd087febdabce8663cfcafce0b17586e4
Michele Di Massimo <167227714+micheledimassimo@users.noreply.github.com>
2024-09-09 16:27:58 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+
+export default {
+  data() {
+    return { 
+      count: 0 
+    }
+  },
+  components: {
+    AppHeader
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppHeader />
+    <button @click="incrementCount()">
+      {{ count }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
